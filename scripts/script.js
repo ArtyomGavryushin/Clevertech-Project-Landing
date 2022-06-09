18ae21e641301456bc338413f03be5bfb9fd1dc8
Artem <90931383+Artem27Rezt@users.noreply.github.com>
2022-06-10 02:23:50 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+// 	$('#pagepiling').pagepiling({
+// 		navigation: {
+//             'textColor': '#fff',
+//             'bulletsColor': '#fff',
+//             'position': 'right',
+//             'tooltips': ['section1', 'section2']
+//         },
+// 	});
+// });
