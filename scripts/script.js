aabf6de70a5930a1d0381852a16461a5cc572564
Artem <90931383+Artem27Rezt@users.noreply.github.com>
2022-06-10 22:23:39 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-// $(document).ready(function() {
-// 	$('#pagepiling').pagepiling({
-// 		navigation: {
-//             'textColor': '#fff',
-//             'bulletsColor': '#fff',
-//             'position': 'right',
-//             'tooltips': ['section1', 'section2']
-//         },
-// 	});
-// });
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+		navigation: {
+            'textColor': '#fff',
+            'bulletsColor': '#fff',
+            'position': 'right',
+            'tooltips': ['section1', 'section2']
+        },
+	});
+});
