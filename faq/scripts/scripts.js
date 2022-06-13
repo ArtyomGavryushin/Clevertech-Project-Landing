a480cf24b01c6cb0803c783e0e481c366110a885
Artem <90931383+Artem27Rezt@users.noreply.github.com>
2022-06-13 21:29:51 +0300

    Add files via upload

---
+++ faq/scripts/scripts.js
@@ -0,0 +1,47 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.question'),
+          tabsContent = document.querySelectorAll('.answer'),
+          tabsParent = document.querySelector('.questions'),
+          tabsContentParent = document.querySelector('.answers'),
+          arrowBack = document.querySelectorAll('.img_arrow_back');
+
+    console.log(arrowBack);
+
+    function hideTabContent(){
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('answer');
+        });
+    }
+
+    function showTabContent(i = 0){
+        tabsContent[i].classList.add('answer');
+        tabsContent[i].classList.remove('hide');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        event.preventDefault();
+
+        if(target && target.classList.contains('question')){
+            tabs.forEach((item, i) => {
+                if(target == item){
+                    hideTabContent(tabsParent.classList.add('hide'));
+                    showTabContent(i, tabsContentParent.classList.remove('hide'));
+                }
+            });
+        }
+    });
+
+    arrowBack.forEach(item => {
+        item.addEventListener('click', () => {
+            tabsContentParent.classList.add('hide');
+            tabsParent.classList.remove('hide');
+         })
+    });
+    
+});
