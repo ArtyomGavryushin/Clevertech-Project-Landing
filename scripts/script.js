53296e03a13188680074ff4512eddf6938cb434b
Artem Gavryushin <90931383+Artem27Rezt@users.noreply.github.com>
2022-06-15 23:43:47 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	if(window.screen.width > 450){
 		navig.classList.remove('hide');
-	}else if(window.screen.width > 300 || window.screen.width < 500){
+	}else if(window.screen.width > 300 && window.screen.width < 500){
 		menu.classList.remove('hide');
 
 		headerText.innerHTML = `<h1 class="header_text">CLEVERTECH MORE THAN JUST<br>WEB 3.0</h1>`;
